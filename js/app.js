400e30cf91b8a29cc93567cb05f6a1e6d8cef08f
Abi Vickery <vickeryabi@gmail.com>
2019-12-09 13:24:57 -0600

    letter counter

--- js/app.js
+++ js/app.js
@@ -119,12 +119,94 @@ for (const key in food) {
             break;
         }
     }
+    console.log(key);
 }
-
-// BREAK, CONTINUE
 
 const iterable = 'boo';
 
 for  (const value of iterable) {
     console.log(value);
 }
+
+// BREAK, CONTINUE
+
+for (const key in food) {
+    if (key == 1) {
+        continue;
+    }
+    console.log('key', key);
+}
+for (const value of person) {
+    console.log(value);
+}
+
+// var data = [
+//     ['Abigail','Vickery'],
+//     ['Abby','Curry']
+// ];
+// var counter = 0;
+// for (let i = 0; i < 10; i++) {
+//     // do stuff
+// }
+
+for (let a = 0; a < Array.length; a++) {
+    for (let b = 0; b < Array.length; b++) {
+        for (let c = 0; c < Array.length; c++) {
+            for (let d = 0; d < Array.length; d++) {
+                for (let e = 0; e < Array.length; e++) {
+                    for (let f = 0; f < Array.length; f++) {
+                        for (let g = 0; g < Array.length; g++) {
+                            for (let h = 0; h < Array.length; h++) {
+                                for (let i = 0; i < Array.length; i++) {
+                                    for (let j = 0; j < Array.length; j++) {
+                                        for (let k = 0; k < Array.length; k++) {
+                                            for (let l = 0; l < Array.length; l++) {
+                                                for (let m = 0; m < Array.length; m++) {
+                                                    for (let n = 0; n < Array.length; n++) {
+                                                        for (let o = 0; o < Array.length; o++) {
+                                                            for (let p = 0; p < Array.length; p++) {
+                                                                for (let q = 0; q < Array.length; q++) {
+                                                                    for (let r = 0; r < Array.length; r++) {
+                                                                        for (let s = 0; s < Array.length; s++) {
+                                                                            for (let t = 0; t < Array.length; t++) {
+                                                                                for (let u = 0; u < Array.length; u++) {
+                                                                                    for (let v = 0; v < Array.length; v++) {
+                                                                                        for (let w = 0; w < Array.length; w++) {
+                                                                                            for (let x = 0; x < Array.length; x++) {
+                                                                                                for (let y = 0; y < Array.length; y++) {
+                                                                                                    for (let z = 0; z < Array.length; z++) {
+                                                                                                        // do stuff
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+let length = 9;
+while (length < 10) {
+    console.log(length);
+    length = 10;
+}
+
+// letter counter
